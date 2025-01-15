459101a27fdda8a194183747c02422ad56feac91
Rahulwinner <rk8670056@gmail.com>
2025-01-15 22:55:30 +0530

    number is negative or positive

---
+++ if_condition/negative_or_positve_number.py
@@ -0,0 +1,8 @@
+try:
+    num1 = int(input("Enter the number...."))
+    if num1>0:
+        print("this is positive number...")
+    else:
+        print("this is negative number...")
+except:
+    print("please enter a valid credential")
